8facac4be1c102fd9917a8d85d1d5a7ab34ea14d
中尾吏志 <nakaosatoshi@nakaorishi-no-MacBook-Pro.local>
2020-07-07 22:50:45 +0900

    add some return value

--- projects/Textfile analysis/textfile_analysis.py
+++ projects/Textfile analysis/textfile_analysis.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import collections
+import string
+import re
 
 script_name = sys.argv[0]
 
@@ -15,15 +17,17 @@ res = {
 
 try:
     textfile = sys.argv[1]
-    with open(textfile) as f:
-        data = f.read()
+    with open(textfile, "r", encoding = "utf_8") as f:
 
+        data = f.read()
         res["total_lines"] = data.count(os.linesep)
-        res["total_characters"] = len(data)
+        res["total_characters"] = len(data.replace(" ","")) - res["total_lines"]
         counter = collections.Counter(data.split())
         d = counter.most_common()
         res["total_words"] = sum([i[1] for i in d])
         res["unique_words"] = len([i[0] for i in d])
+        special_chars = string.punctuation
+        res["special_characters"] = sum(v for k, v in collections.Counter(data).items() if k in special_chars)
 
 except IndexError:
     print('Usage: %s TEXTFILE' % script_name)
