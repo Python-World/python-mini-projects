2c5f14f34b4bcc8cbff963c375009476ebdecef9
ShivSt <sachinraina07@gmail.com>
2020-07-23 15:50:51 +0530

    changed code formating

--- projects/check website connectivity/check_connectivity.py
+++ projects/check website connectivity/check_connectivity.py
@@ -1,18 +1,17 @@
 import requests
 import csv
 
-status_dict={'Website': 'Status'}
+status_dict = {"Website": "Status"}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	with open('websites.txt', 'r') as fr:
-		for line in fr:
-			status=requests.get(line.strip()).status_code
-			status_dict[line] = 'working' if status==200 else 'not working'
+    with open("websites.txt", "r") as fr:
+        for line in fr:
+            status = requests.get(line.strip()).status_code
+            status_dict[line] = "working" if status == 200 else "not working"
 
-	print(status_dict)
-	with open('website_status.csv', 'w', newline='') as fw:
-		csv_writers = csv.writer(fw)
-		for key in status_dict.keys():
-			csv_writers.writerow([key, status_dict[key]])
-	
+    print(status_dict)
+    with open("website_status.csv", "w", newline="") as fw:
+        csv_writers = csv.writer(fw)
+        for key in status_dict.keys():
+            csv_writers.writerow([key, status_dict[key]])
