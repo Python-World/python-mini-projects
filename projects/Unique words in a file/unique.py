b06e011436df9ac253564bb1e5e36fc69f7196fe
Rakshit Puri <32843491+skate1512@users.noreply.github.com>
2020-07-24 19:11:38 +0530

    Added option to input file name

--- projects/Unique words in a file/unique.py
+++ projects/Unique words in a file/unique.py
@@ -1,6 +1,10 @@
 import re
 
 list_of_words = []
+
+# Alternate Method to insert file
+# file = input("Enter file name: ")
+# with open(file, "r") as f:
 with open("text_file.txt", "r") as f:
     for line in f:
         list_of_words.extend(re.findall(r"[\w']+", line))
