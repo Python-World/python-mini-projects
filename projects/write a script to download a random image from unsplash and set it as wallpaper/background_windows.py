3d3992791ea4adba267d770e26a0bc87db026357
kana800 <inuxxc@tutanota.com>
2020-07-29 09:28:47 -0400

    update to change-background script

--- projects/write a script to download a random image from unsplash and set it as wallpaper/background_windows.py
+++ projects/write a script to download a random image from unsplash and set it as wallpaper/background_windows.py
@@ -1,10 +1,13 @@
 from requests import get
 import os
 import ctypes
+import argparse
 
 url = "https://source.unsplash.com/random"
 filename = "random.jpg"
-
+parser = argparse.ArgumentParser(description="Change the Desktop Background")
+parser.add_argument('version',type = str,help="Windows Version (32 or 64)")
+args = parser.parse_args()
 
 def download(url, file_name):
     '''
@@ -15,13 +18,19 @@ def download(url, file_name):
         file.write(response.content)
 
 
-def setup(pathtofile):
+def setup(pathtofile,version):
     name_of_file = pathtofile
     path_to_file = os.path.join(os.getcwd(), name_of_file)
     SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path_to_file, 0)
+    if version == "32":
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path_to_file, 0)
+    elif version == "64":
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_to_file, 0)
 
 
 if __name__ == "__main__":
-    download(url, filename)
-    setup(filename)
+    try:
+        download(url, filename)
+        setup(filename,args.version)
+    except Error as e:
+        raise NotImplementedError
