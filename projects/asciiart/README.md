6bfccd24f9242f4fbd8f98a84a47594cdbfe6e43
Akash <akasharul2101@gmail.com>
2020-08-02 06:52:18 +0530

    Add README and basic template of the script

---
+++ projects/asciiart/README.md
@@ -0,0 +1,2 @@
+# ascii-art
+this script will take in an image and print out an ASCII-art.
---
+++ projects/asciiart/make_art.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import cv2
+
+def draw(image):
+    pass
+
+if __name__ == '__main__':
+    image_path = 'sample_image.png'
+    image = cv2.imread(image_path)
+
+    draw(image)
