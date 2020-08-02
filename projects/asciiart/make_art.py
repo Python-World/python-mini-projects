eace37ffe53556c2f7f178bc27424a4df79b5ade
Akash <akasharul2101@gmail.com>
2020-08-02 09:55:46 +0530

    Add comments to make_art.py

--- projects/asciiart/make_art.py
+++ projects/asciiart/make_art.py
@@ -3,36 +3,51 @@
 import cv2
 import numpy as np
 
-def print_out(array):
-	'''prints the array line by line'''
+import sys
 
-	symbols = ['#','-','*','.','+','o']
-	for row in array:
-		for e in row:
-			print(symbols[int(e)%len(symbols)], end='')
-		print()
+
+def print_out_ascii(array):
+    """prints the coded image with symbols"""
+
+    symbols = ["#", "-", "*", ".", "+", "o"]
+    for row in array:
+        for e in row:
+        	# select symbol based on the type of coding
+            print(symbols[int(e) % len(symbols)], end="")
+        print()
+
 
 def img_to_ascii(image):
     """returns the numeric coded image"""
-    
-    width, height = image.shape
-    width = int(width/18)
-    height = int(height/10)
-    # resize image to fit the printing screen
-    resized_image = cv2.resize(
-        image, (height, width),
-    )  
 
-    threshold_list = [0,50,100,150,200]
+    # resizing parameters
+    height, width = image.shape
+    new_width = int(width / 12)
+    new_height = int(height / 18)
+
+    # resize image to fit the printing screen
+    resized_image = cv2.resize(image, (new_width, new_height),)
+
+    threshold_list = [0, 50, 100, 150, 200]  # list of threshold values
     thresh_image = np.zeros(resized_image.shape)
-    
+
     for i, threshold in enumerate(threshold_list):
-    	thresh_image[resized_image > threshold] = i
+        # assign corresponding values according to the index of threshold applied
+        thresh_image[resized_image > threshold] = i
     return thresh_image
 
+
 if __name__ == "__main__":
-    image_path = '/home/akash/Downloads/2017505508.jpg' 
-    image = cv2.imread(image_path, 0)
+
+    if len(sys.argv) < 2:
+        print("Image Path not specified : Using sample_image.png\n")
+        image_path = "sample_image.png"  # default image path
+
+    if len(sys.argv) == 2:
+        print("Using {} as Image Path\n".format(sys.argv[1]))
+        image_path = sys.argv[1]
+
+    image = cv2.imread(image_path, 0)  # read image
 
     ascii_art = img_to_ascii(image)
-    print_out(ascii_art)
+    print_out_ascii(ascii_art)
