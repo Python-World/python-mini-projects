63df27fc0ba2a8cff910861eb5442e9c8afe3d6a
Akash <akasharul2101@gmail.com>
2020-08-02 07:18:30 +0530

    print a basic binary ascii art of only two symbols

--- projects/asciiart/make_art.py
+++ projects/asciiart/make_art.py
@@ -2,12 +2,31 @@
 
 import cv2
 
-def draw(image):
-    '''prints the ascii art of the given image'''
-    pass
+def print_out(array):
+	'''prints the array line by line'''
+	for row in array:
+		for e in row:
+			if e:
+				print('-', end='')
+			else:
+				print('|', end='')
+		print()
 
-if __name__ == '__main__':
-    image_path = 'sample_image.png'
-    image = cv2.imread(image_path)
+def img_to_ascii(image):
+    """converts the image to ascii art"""
+    
+    resized_image = cv2.resize(
+        image, (50, 50)
+    )  # resize image to fit the printing screen
+    _, thresh = cv2.threshold(
+        resized_image, 127, 1, 1
+    )  # apply threshold to substitute different symbols
 
-    draw(image)
+    return thresh
+
+if __name__ == "__main__":
+    image_path = "sample_image.png"
+    image = cv2.imread(image_path, 0)
+
+    ascii_art = img_to_ascii(image)
+    print_out(ascii_art)
