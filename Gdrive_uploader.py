8cab3ab63cf7519f1cd5a796d3577984c3b677ec
Giridharsalana <37552476+Giridharsalana@users.noreply.github.com>
2020-08-06 18:57:53 +0530

    Update Gdrive_uploader.py
    Added Instruction

--- Gdrive_uploader.py
+++ Gdrive_uploader.py
@@ -2,6 +2,29 @@
 Python Gdrive uploader using pydrive
 It will upload all files in a "Dir_Path" folder
 '''
+'''
+INSTRUCTION TO USE
+
+
+#Authentication
+
+Drive API requires OAuth2.0 for authentication. PyDrive makes your life much easier by handling complex authentication steps for you.
+
+1.Go to APIs Console and make your own project.
+2.Search for ‘Google Drive API’, select the entry, and click ‘Enable’.
+3.Select ‘Credentials’ from the left menu, click ‘Create Credentials’, select ‘OAuth client ID’.
+4.Now, the product name and consent screen need to be set -> click ‘Configure consent screen’ and follow the instructions. Once finished:
+5.Select ‘Application type’ to be Web application.
+6.Enter an appropriate name.
+7.Input http://localhost:8080 for ‘Authorized JavaScript origins’.
+8.Input http://localhost:8080/ for ‘Authorized redirect URIs’.
+9.Click ‘Save’.
+10.Click ‘Download JSON’ on the right side of Client ID to download client_secret_<really long ID>.json.
+The downloaded file has all authentication information of your application. Rename the file to “client_secrets.json” and place it in the directory where your py script is there.
+
+'''
+
+
 
 import os
 from pydrive.auth import GoogleAuth
