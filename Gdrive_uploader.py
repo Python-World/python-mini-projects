260e7dabb928fb12075ec65dd8b33a42c153fde5
Giridharsalana <37552476+Giridharsalana@users.noreply.github.com>
2020-08-06 18:32:34 +0530

    Update Gdrive_uploader.py
    Completed script.

--- Gdrive_uploader.py
+++ Gdrive_uploader.py
@@ -1,4 +1,22 @@
 '''
-Author:-Giridhar Salana
-Email :-giridharsalana@gmail.com
+Python Gdrive uploader using pydrive
+It will upload all files in a "Dir_Path" folder
 '''
+
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+Dir_Path = r"C:\Users\Admin\Desktop\Test"
+
+for x in os.listdir(path): 
+   
+    f = drive.CreateFile({'title': x}) 
+    f.SetContentFile(os.path.join(path, x)) 
+    f.Upload() 
+    f = none #pydrive bugfix Line
