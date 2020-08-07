d55f9dc95a11fd4bcddb3686415e027939c554da
Jacob Ogle <65563124+JakeOgle94@users.noreply.github.com>
2020-08-07 15:14:18 -0400

    Update internet_connection_check.py
    Updated the tab indentations

--- projects/Internet Connection Check/internet_connection_check.py
+++ projects/Internet Connection Check/internet_connection_check.py
@@ -1,20 +1,16 @@
 import requests
 
-def Internet_Connection_Test():
-	
-    '''This function will attempt to connect to Google to check the status of the internet''' 
-    
-    
-    url = 'https://www.google.com/'
-	
+def internet_conntection_test():
+	url = 'https://www.google.com/'
 	print(f'Attempting to connect to {url} to determine internet connection status.')
 	
-    try:
+	try:
 		requests.get(url, timeout = 10)
 		print(f'Connection to {url} was successful.')
 		return True
-	except requests.ConnectionError:
+	except:
+		requests.ConnectionError
 		print(f'Failed to connect to {url}.')
 		return False
-
-Internet_Connection_Test()
+	
+internet_connection_test()
