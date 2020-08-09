83092126038b5f3364996f79ab98c153883134c5
Aditya Jetely <ajetely@gmail.com>
2020-08-09 16:36:07 +0530

    Fixed Time Issue

--- projects/Digital Clock/digital_clock.py
+++ projects/Digital Clock/digital_clock.py
@@ -37,6 +37,15 @@ canvas.pack()
 
 frame = tk.Frame(root, bg='#22478a')
 frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+lbl = tk.Label(frame, font=('calibri', 40, 'bold'),
+                     background='#22478a', foreground='black')
+lbl.pack(anchor="s")
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+time()
 
 menubar = tk.Menu(root)
 theme_menu = tk.Menu(menubar, tearoff=0)
