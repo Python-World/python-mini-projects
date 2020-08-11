5f41e29d6af230f94b851600bcb582eee196fb3b
Phillibob55 <itsluqman55@gmail.com>
2020-08-11 19:54:07 +0500

    Final

--- projects/Website Blocker/website_blocker.py
+++ projects/Website Blocker/website_blocker.py
@@ -1,17 +1,12 @@
-import time
-from datetime import datetime as dt
-
-#hostsPath="hosts"
-hostsPath=r"C:\Windows\System32\drivers\etc\hosts"
-
+pathToHosts=r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 websites=["https://www.sislovesme.com/","https://motherless.com/","https://xhamster.com/","https://www.xnxx.com/","https://www.xvideos.com/","https://www.pornhub.com/"]
 
-with open(hostsPath,'r+') as file:
-    content=file.read()
-    for site in websites:
-        if site in content:
-            pass
-        else:
-            file.write(redirect+" "+site+"\n")
+with open(pathToHosts,'r+') as file:
+content=file.read()
+for site in websites:
+    if site in content:
+        pass
+    else:
+        file.write(redirect+" "+site+"\n")
             
--- projects/Website Blocker/website_unblocker.py
+++ projects/Website Blocker/website_unblocker.py
@@ -1,16 +1,11 @@
-import time
-from datetime import datetime as dt
-
-#hostsPath="hosts"
-hostsPath=r"C:\Windows\System32\drivers\etc\hosts"
-
+pathToHosts=r"C:\Windows\System32\drivers\etc\hosts"
 websites=["https://www.sislovesme.com/","https://motherless.com/","https://xhamster.com/","https://www.xnxx.com/","https://www.xvideos.com/","https://www.pornhub.com/"]
 
-with open(hostsPath,'r+') as file:
-            content=file.readlines()
-            file.seek(0)
-            for line in content:
-                if not any(site in line for site in websites):
-                    file.write(line)
-            file.truncate()
+with open(pathToHosts,'r+') as file:
+content=file.readlines()
+file.seek(0)
+for line in content:
+    if not any(site in line for site in websites):
+        file.write(line)
+file.truncate()
             
