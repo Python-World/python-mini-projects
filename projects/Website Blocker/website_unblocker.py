7e35c155155711025a83c1bca3907f37a6552e39
Phillibob55 <itsluqman55@gmail.com>
2020-08-16 09:54:57 +0500

    fixed
    1) Removed the indent error
    2) Made linux compatible

--- projects/Website Blocker/website_unblocker.py
+++ projects/Website Blocker/website_unblocker.py
@@ -1,11 +1,18 @@
+import platform
+
+if platform.system() == "Windows":
+        pathToHosts=r"C:\Windows\System32\drivers\etc\hosts"
+elif platform.system() == "Linux":
+        pathToHosts=r"~\etc\hosts"
+
 pathToHosts=r"C:\Windows\System32\drivers\etc\hosts"
 websites=["https://www.sislovesme.com/","https://motherless.com/","https://xhamster.com/","https://www.xnxx.com/","https://www.xvideos.com/","https://www.pornhub.com/"]
 
 with open(pathToHosts,'r+') as file:
-content=file.readlines()
-file.seek(0)
-for line in content:
-    if not any(site in line for site in websites):
-        file.write(line)
-file.truncate()
+    content=file.readlines()
+    file.seek(0)
+    for line in content:
+        if not any(site in line for site in websites):
+            file.write(line)
+    file.truncate()
             
