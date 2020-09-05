3a56705f3b13a7b35f28eb1dd6856c123dcabc02
عرفان صابری | Erfan Saberi <55283540+erfansaberi@users.noreply.github.com>
2020-09-05 12:12:07 +0430

    Create generator.py

---
+++ projects/recursive password generator/generator.py
@@ -0,0 +1,17 @@
+import random
+
+def stretch(text,maxlength):
+    if len(text) < maxlength:
+        randomChar = get_random_char()
+        return stretch(text+randomChar,maxlength)
+    else:
+        return text
+
+def get_random_char():
+    chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?'
+    randomChar = chars[random.randint(0,len(chars)-1)]
+    return randomChar
+
+while 1:
+    maxlength = int(input(' [?] Enter a length for your password: '))
+    print(' [+]',stretch('',maxlength),'\n')
