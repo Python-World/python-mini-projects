8010e96cecefc72c9cef4d46491c84353979bce8
Aditya Jetely <ajetely@gmail.com>
2020-09-07 15:40:24 +0530

    Added script to check website load time

---
+++ projects/Time to load Website/time_to_load_website.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+import time
+
+#Storing user defined URl
+URL = input("Enter the url whose loading time you want to check: ")
+
+#Open the url as entered by the user and checking protocols
+if ("https" or "http") in URL:
+    open_this_url = urlopen(URL)
+
+else:
+    open_this_url = urlopen("https://" + URL)
+
+#Time stamp before the reading of url starts
+start_time = time.time()
+
+#Reading the user defined url
+read_the_url = open_this_url.read()
+
+#Time stamp after the reading of the url 
+end_time = time.time()
+
+#Closing the instance of the urlopen object
+open_this_url.close()
+
+print()
+print(f"The time taken to load {URL} is {end_time - start_time}")
