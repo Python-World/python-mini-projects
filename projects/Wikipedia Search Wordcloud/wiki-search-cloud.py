96380b8874e69095a64dedf0fbcf39046168efbe
Naman Shah <64421164+namanshah01@users.noreply.github.com>
2020-09-08 17:04:10 +0530

    minor changes

--- projects/Wikipedia Search Wordcloud/wiki-search-cloud.py
+++ projects/Wikipedia Search Wordcloud/wiki-search-cloud.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 import wikipedia
 import sys
 import warnings
+# supressing unnecessary warnings
 warnings.filterwarnings("ignore")
 
+# function to search the wikipedia article and generate the wordcloud
 def gen_cloud(topic):
     try:
-        content = str(wikipedia.page(topic).content[:5000])
+        content = str(wikipedia.page(topic).content)
     except:
         print("Error, try searching something else...")
         sys.exit()
@@ -16,14 +18,17 @@ def gen_cloud(topic):
     wordcloud = WordCloud(stopwords=stopwords, max_words=200, background_color="black", width=600, height=350).generate(content)
     return wordcloud
 
+# function to save the wordcloud to current directory
 def save_cloud(wordcloud):
     wordcloud.to_file("./wordcloud.png")
 
+# function to display the wordcloud with matplotlib
 def show_cloud(wordcloud):
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
 
+# driver code
 if __name__ == '__main__':
     topic = input("What do you want to search: ").strip()
     wordcloud = gen_cloud(topic)
