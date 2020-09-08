04b48eebd9492611290ae87e0d095941ab791850
Aditya Jetely <ajetely@gmail.com>
2020-09-08 11:30:05 +0530

    Followed PEP-8 rules

--- projects/Time to load Website/time_to_load_website.py
+++ projects/Time to load Website/time_to_load_website.py
@@ -1,26 +1,39 @@
 from urllib.request import urlopen
 import time
 
-#Storing user defined URl
-URL = input("Enter the url whose loading time you want to check: ")
 
-#Open the url as entered by the user and checking protocols
-if ("https" or "http") in URL:
-    open_this_url = urlopen(URL)
+def get_load_time(url):
+    """This function takes a user defined url as input
+    and returns the time taken to load that url in seconds.
 
-else:
-    open_this_url = urlopen("https://" + URL)
+    Args:
+        url (string): The user defined url.
 
-#Time stamp before the reading of url starts
-start_time = time.time()
+    Returns:
+        time_to_load (float): The time taken to load the website in seconds.
+    """
 
-#Reading the user defined url
-read_the_url = open_this_url.read()
+    if ("https" or "http") in url:  # Checking for presence of protocols
+        open_this_url = urlopen(url)  # Open the url as entered by the user
+    else:
+        open_this_url = urlopen("https://" + url)  # Adding https to the url
 
-#Time stamp after the reading of the url 
-end_time = time.time()
+    start_time = time.time()  # Time stamp before the reading of url starts
 
-#Closing the instance of the urlopen object
-open_this_url.close()
+    open_this_url.read()  # Reading the user defined url
 
-print(f"\nThe time taken to load {URL} is {end_time - start_time} seconds.")
+    end_time = time.time()  # Time stamp after the reading of the url
+
+    open_this_url.close()  # Closing the instance of the urlopen object
+
+    time_to_load = end_time - start_time
+
+    return time_to_load
+
+
+if __name__ == '__main__':
+
+    url = input("Enter the url whose loading time you want to check: ")
+
+    print(
+        f"\nThe time taken to load {url} is {get_load_time(url):.2} seconds.")
