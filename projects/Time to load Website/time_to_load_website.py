68b9e70d0974b7a47183c8166752930e1aacb1ac
Aditya Jetely <ajetely@gmail.com>
2020-09-08 12:18:36 +0530

    Update time_to_load_website.py

--- projects/Time to load Website/time_to_load_website.py
+++ projects/Time to load Website/time_to_load_website.py
@@ -17,22 +17,15 @@ def get_load_time(url):
         open_this_url = urlopen(url)  # Open the url as entered by the user
     else:
         open_this_url = urlopen("https://" + url)  # Adding https to the url
-
     start_time = time.time()  # Time stamp before the reading of url starts
-
     open_this_url.read()  # Reading the user defined url
-
     end_time = time.time()  # Time stamp after the reading of the url
-
     open_this_url.close()  # Closing the instance of the urlopen object
-
     time_to_load = end_time - start_time
 
     return time_to_load
 
 
 if __name__ == '__main__':
-
     url = input("Enter the url whose loading time you want to check: ")
-
     print(f"\nThe time taken to load {url} is {get_load_time(url):.2} seconds.")
