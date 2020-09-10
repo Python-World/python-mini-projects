3307a58f57ec28db49f02b352f8bce87bbc6c98b
Aditya Jetely <ajetely@gmail.com>
2020-09-10 08:30:56 +0530

    Update app.py

--- projects/simple-if-else-dictionary/app.py
+++ projects/simple-if-else-dictionary/app.py
@@ -1,18 +1,19 @@
-#Simple if else Dictionary.
-from json import load
-from difflib import get_close_matches	#if you enter this library will check it.
+import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
+
 
 def translate(word):
     word = word.lower()
     if word in data:
-         result = data[0]
+        result = data[0]
         return result
     elif len(word) == 0:
         return "Please!! Enter word . . . "
     elif len(get_close_matches(word, data.keys())) > 0:
-        yn = input("Did you mean %s instead?\nEnter Y if yes, or N if no : " % get_close_matches(word, data.keys())[0])
-        if (yn.lower()== "y"):
+        yn = input("Did you mean %s instead?\nEnter Y if yes, or N if no : "
+                   % get_close_matches(word, data.keys())[0])
+        if (yn.lower() == "y"):
             return data[get_close_matches(word, data.keys())[0]]
         elif (yn == "N") or (yn == "n"):
             return "Sorry!! The word doesn't exist. Please cross check it.\n"
@@ -21,11 +22,12 @@ def translate(word):
     else:
         return "The word doesn't exist. Please cross check it.\n"
 
-print("Enter word to know meaning!!")
-word = input("Enter Word : ")
-output = translate(word)
-if isinstance(output,list)
-    for item in output:
-        print(item)
-else:
-    print(output)
+
+if __name__ == "__main__":
+    word = input("Enter word to know meaning!!: ")    
+    output = translate(word)
+    if isinstance(output, list):
+        for item in output:
+            print(item)
+    else:
+        print(output)
