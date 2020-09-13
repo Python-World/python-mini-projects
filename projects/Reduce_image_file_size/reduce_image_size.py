75e5aa104c6afc39d422d4d3a8f4ba581064a221
Vipul Verma <vipulverma802@gmail.com>
2020-09-13 16:45:16 +0530

    added comments

--- projects/Reduce_image_file_size/reduce_image_size.py
+++ projects/Reduce_image_file_size/reduce_image_size.py
@@ -1,20 +1,23 @@
+# import openCV library for image handling
 import cv2
 
+# read image to be resized by imread() function of openCV library
 img = cv2.imread('input.jpg')
-
 print(img.shape)
 
-# rescalling image
-width = int((img.shape[1])/5)
-height = int((img.shape[0])/5)
+# set the ratio of resized image
+k = 5
+width = int((img.shape[1])/k)
+height = int((img.shape[0])/k)
 
+# resize the image by resize() function of openCV library
 scaled = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-
 print(scaled.shape)
 
-cv2.imshow("Output",scaled)
+# show the resized image using imshow() function of openCV library
+cv2.imshow("Output", scaled)
 cv2.waitKey(500)
 cv2.destroyAllWindows()
 
-cv2.imwrite('resized_output_image.jpg',scaled)
-
+# get the resized image output by imwrite() function of openCV library
+cv2.imwrite('resized_output_image.jpg', scaled)
