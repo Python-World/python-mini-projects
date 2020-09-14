f9d47b505cd910e542ccdf3829d85cd6491385e2
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2020-09-15 01:27:29 +0530

    Working on genres

--- projects/Find_imdb_rating/find_IMDb_rating.py
+++ projects/Find_imdb_rating/find_IMDb_rating.py
@@ -10,7 +10,9 @@ file1 = open ("ratings.csv", "w")
 
 with open("movies.txt", "r") as file2:
     for line in file2:
-        title = line.lower()
+        x = line.split(", ")
+        title = x[0].lower()
+        release = x[1]
         query = "+".join(title.split())
         URL = "https://www.imdb.com/search/title/?title=" + query
         try: 
@@ -24,23 +26,28 @@ with open("movies.txt", "r") as file2:
                 name1 = result.h3.a.text
                 name = result.h3.a.text.lower()
                 # print(name)
-                # year = result.h3.find(
-                #     "span", class_="lister-item-year text-muted unbold"
-                # ).text.lower()
+                year = result.h3.find(
+                    "span", class_="lister-item-year text-muted unbold"
+                ).text.lower()
 
-                if title in name:
+                # print(name)
+                # print(year)
+                # print(release)
+
+                if title in name and release in year:
                     rating = result.find("div",class_="inline-block ratings-imdb-rating")["data-value"]
                     print(f"Rating of {name1}:", rating)
+                    genres = []
                     genre = result.p.find("span", class_="genre")
+                    for x in genre:
+                        genres.append(x)
                     file1.write(name1)
                     file1.write(',')
                     file1.write(rating)
-                    file1.write(',')
-                    file1.write(genre)
+                    # file1.write(',')
                     file1.write('\n')
                     
-                    # for x in genre:
-                    #     print(x)
+                    
 
 
         except Exception:
