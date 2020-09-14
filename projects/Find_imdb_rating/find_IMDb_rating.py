5475eb71f7d526ed09b9552239ff1738025eb21a
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2020-09-14 14:47:13 +0530

    Fetching IMDB Rating Completed

--- projects/Find_imdb_rating/find_IMDb_rating.py
+++ projects/Find_imdb_rating/find_IMDb_rating.py
@@ -1,38 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 
 # Defining variables and url
 title = str(input("Enter the title of movie/series: "))
-release = str(input("Enter the year of release: "))
+year = str(input("Enter the year of release: "))
 query = "+".join(title.split())
-#RL = f"https://www.imdb.com/search/title/?title={query}"
 
 request_link = 'http://www.omdbapi.com/?i=tt3896198&apikey=2c84f11f'
 
-#api_key = 2c84f11f
+#final request link using OMDb API
+request = request_link + '&t=' + query + '&y=' + year
 
-request = request_link + '&t=' + query
-result = requests.get(request)
+#requesting data from OMBd
+response = requests.get(request)
 
-print(result.text)
+#converting to json (dictionary)
+response = response.json()
 
-# s = requests.session()  # Setting up session
-
-# try:
-#     response = s.get(URL)
-#     soup = BeautifulSoup(response.content, features="lxml")
-#     containers = soup.find_all("div", class_="lister-item-content")
-
-#     for result in containers:
-#         name = result.h3.a.text.lower()
-#         year = result.h3.find(
-#             "span", class_="lister-item-year text-muted unbold"
-#         ).text.lower()
-
-#         if title in name and release in year:
-#             rating = result.find("div",
-#                                  class_="inline-block ratings-imdb-rating")[
-#                                  "data-value"]
-#             print(f"Rating of {name}:", rating)
-# except Exception:
-#     print("Try again with valid combination of tile and release year")
+#printing imdb rating
+print(response['imdbRating'])
