68794d804d21a6c69e0008a61262dec386663351
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2020-09-15 15:40:23 +0530

    Web scraping and putting content in CSV Working

--- projects/Find_imdb_rating/find_IMDb_rating.py
+++ projects/Find_imdb_rating/find_IMDb_rating.py
@@ -1,57 +1,68 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import pandas as pd
 
 # Setting up session
 s = requests.session()  
 
 #File to write movies into 
-file1 = open ("ratings.csv", "w")
 
-with open("movies.txt", "r") as file2:
-    for line in file2:
-        x = line.split(", ")
-        title = x[0].lower()
-        release = x[1]
-        query = "+".join(title.split())
-        URL = "https://www.imdb.com/search/title/?title=" + query
-        try: 
-            response = s.get(URL)
+films = ['Star Wars: Episode V - The Empire Strikes Back', 'The wolf of wall street', 'Shutter Island']
 
-            content = response.content
+names = []
+ratings = []
+genres = []
 
-            soup = BeautifulSoup(response.content)
-            containers = soup.find_all("div", class_="lister-item-content")
-            for result in containers:
-                name1 = result.h3.a.text
-                name = result.h3.a.text.lower()
-                # print(name)
-                year = result.h3.find(
-                    "span", class_="lister-item-year text-muted unbold"
-                ).text.lower()
+for line in films:
+    # x = line.split(", ")
+    title = line.lower()
+    # release = x[1]
+    query = "+".join(title.split()) 
+    URL = "https://www.imdb.com/search/title/?title=" + query
+    print(URL)
+    # print(release)
+    try: 
+        response = s.get(URL)
 
-                # print(name)
-                # print(year)
-                # print(release)
+        #getting contect from IMDB Website
+        content = response.content
 
-                if title in name and release in year:
-                    rating = result.find("div",class_="inline-block ratings-imdb-rating")["data-value"]
-                    print(f"Rating of {name1}:", rating)
-                    genres = []
-                    genre = result.p.find("span", class_="genre")
-                    for x in genre:
-                        genres.append(x)
-                    file1.write(name1)
-                    file1.write(',')
-                    file1.write(rating)
-                    # file1.write(',')
-                    file1.write('\n')
-                    
-                    
+        # print(response.status_code)
+
+        soup = BeautifulSoup(response.content, features="html.parser") 
+        #searching all films containers found
+        containers = soup.find_all("div", class_="lister-item-content")
+        for result in containers:
+            name1 = result.h3.a.text
+            name = result.h3.a.text.lower()
+
+            # Uncomment below lines if you want year specific as well, define year variable before this 
+            # year = result.h3.find(
+            # "span", class_="lister-item-year text-muted unbold"
+            # ).text.lower() 
+
+            #if film found (searching using name)
+            if title in name:
+                #scraping rating
+                rating = result.find("div",class_="inline-block ratings-imdb-rating")["data-value"]
+                # print(f"Rating of {name1}:", rating)
+                #scraping genre
+                genre = result.p.find("span", class_="genre")
+                genre = genre.contents[0]
+
+                #appending name, rating and genre to individual lists
+                names.append(name1)
+                ratings.append(rating)
+                genres.append(genre)
+                
 
 
-        except Exception:
-            print("Try again with valid combination of tile and release year")
+    except Exception:
+        print("Try again with valid combination of tile and release year")
 
-file1.close()
-    
+#storing in pandas dataframe
+df = pd.DataFrame({'Film Name':names,'Rating':ratings,'Genre':genres}) 
+
+#making csv using pandas
+df.to_csv('products.csv', index=False, encoding='utf-8')
