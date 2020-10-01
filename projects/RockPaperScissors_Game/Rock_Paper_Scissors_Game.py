93229b43b15bbf53909b4b2dba431a64c4dd1346
Ruchi Jain <49330032+ruchijain4@users.noreply.github.com>
2020-10-01 17:16:46 +0530

    New code for this file
    wrote a better and more understandable code for this game

--- projects/RockPaperScissors_Game/Rock_Paper_Scissors_Game.py
+++ projects/RockPaperScissors_Game/Rock_Paper_Scissors_Game.py
@@ -1,60 +1,36 @@
-#START;
-
 import random
-
-#DEFAULT;
-my_dict={'R':"Rock",'P':"Paper",'S':"Scissors"}
-user_count=0
-comp_count=0
-
-#INPUT;
-games=int(input("\nEnter the number of games you want to play: "))
-
-while(comp_count+user_count<games):
-	#WHILE LOOP STARTS;
-
-	flag=0
-
-	user_input=input("\nUser's Input: ")[0]
-	user_input=user_input.upper()
-	#The [0] after the input() will assign the first charcter of input to the variable;
-	#Hence, the user can enter anything, anyway; 
-	#Example: The user can enter Rock or rock or r or R or ro or any such thing which represents Rock;
-	#It will always take input as a R
-	#Thereby, increasing the user input window;
-
-	for i in my_dict.keys():
-		if(user_input==i):		#If the entered input is confined to Rock, Paper or Scissors;
-			flag=1
-			break
-	if(flag!=1):				#If not, run the loop again;
-		print("INVALID INPUT")
-		continue
-
-	comp_input=random.choice(list(my_dict.keys()))	#Random Key from the dictionary my_dict i.e. R,P or S;
-
-	print("Computer's Input: ", my_dict[comp_input])
-	if ( user_input=='R' and comp_input=='P' ) or ( user_input=='P' and comp_input=='S' ) or ( user_input=='S' and comp_input=='R' ):
-		comp_count+=1
-	elif ( user_input=='P' and comp_input=='R' ) or ( user_input=='S' and comp_input=='P' ) or ( user_input=='R' and comp_input=='S' ):
-		user_count+=1
-	else:
-		print("TIE")
-
-	print("\nSCORE:")
-	print("User Score:",user_count,"\tComputer Score:",comp_count,"\n")
-
-	#LOOP ENDS;
-
-print("\n\t\tFINAL SCORE:")
-print("User Score:",user_count,"\t\t\tComputer Score:",comp_count,"\n")
-if user_count>comp_count:
-	print("\n\tCONGRATULATIONS! YOU WON!")
-elif user_count<comp_count:
-	print("\n\t\tSORRY! YOU LOST!")
-else:
-	print("\n\t\tOOPS! IT'S A TIE!")
-
+print("---------Rock Paper Scissors---------")
+while True:
+    print("Enter \n 1 for Rock \n 2 for Paper \n 3 for Scissors")
+    userchoice = int(input("Enter your choice: "))
+    while userchoice > 3 or userchoice < 1 :
+        userchoice = int(input("Enter valid input: "))
+    choice = ['Rock', 'Paper', 'Scissors']
+    print("User choice is " + choice[userchoice - 1])
+    botchoice = random.choice(choice)
+    print("Bot choice is " + botchoice)
+    if choice[userchoice - 1] == botchoice :
+        print("It's a tie")
+    else :
+        if choice[userchoice - 1] == 'Rock' :
+            if botchoice == 'Paper' :
+                print("Bot wins")
+            if botchoice == 'Scissors' :
+                print("You wins")
+        if choice[userchoice - 1] == 'Paper' :
+            if botchoice == 'Rock' :
+                print("You wins")
+            if botchoice == 'Scissors' :
+                print("Bot wins")
+        if choice[userchoice - 1] == 'Scissors' :
+            if botchoice == 'Paper' :
+                print("You win")
+            if botchoice == 'Rock' :
+                print("Bot win")
+    re = input("Press N to quit playing and any other key to continue:  ")
+    if re == 'n' or re == 'N' :
+        break
+print("Thanks for playing")
 
 
 #END;
