25ef72ecd9b57fe890b8a081d1fa8b0d9498203d
ShravanBhat <2017.shravan.bhat@ves.ac.in>
2020-10-02 17:08:37 +0530

    hashing with screenshot

--- projects/Hash_a_given_message/hash.py
+++ projects/Hash_a_given_message/hash.py
@@ -1,17 +1,19 @@
 import hashlib
 def has():
+    #List available algorithms
     print("Available algorithms are: ")
     q=sorted(list(hashlib.algorithms_available))
     for i in range(len(q)):
         print('{0: <20}'.format(q[i]),end="")
         if i%3==2:
             print()
-    str2hash = list(input("Enter the names of the algorithms separated by space : ").split())
-    stri=input("Enter the message: ")
+    str2hash = list(input("Enter the names of the algorithms separated by space : ").split()) #Take input of algorithm names separated by space
+    stri=input("Enter the message: ") #Enter the message to be encoded
     for i in str2hash:
         i=i.lower()
-        if i in list(hashlib.algorithms_available):
-            result = hashlib.new(i)
+        
+        if i in list(hashlib.algorithms_available): #check whether names provided by user is valid or not
+            result = hashlib.new(i) 
             result.update(stri.encode())
             print("The hexadecimal equivalent of "+i+" hash is : ", end ="") 
             print(result.hexdigest())
