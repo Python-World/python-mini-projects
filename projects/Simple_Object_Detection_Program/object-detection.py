6ae4ec841d44afafe1e1cb428dc6fdc49b8f8ae2
akhilaku <akhildasks1999@gmail.com>
2020-10-17 20:21:52 +0530

    modiefied the code by adding comments

--- projects/Simple_Object_Detection_Program/object-detection.py
+++ projects/Simple_Object_Detection_Program/object-detection.py
@@ -1,9 +1,9 @@
-import cv2
-import matplotlib.pyplot as plt
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
-im = cv2.imread('img1.jpg')
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
+import cv2                             #importing Opencv2
+import matplotlib.pyplot as plt        #importing the matplotlib
+import cvlib as cv                     #importing the opencv library for object detection.
+from cvlib.object_detection import draw_bbox          #importing draw_bbox(for drawing the box around detected object)
+im = cv2.imread('img1.jpg')                           #input image(you can replace 'img1.jpg' with your image)
+bbox, label, conf = cv.detect_common_objects(im)      #code for the detection of objects
+output_image = draw_bbox(im, bbox, label, conf)       #code for pre-presenting the image
+plt.imshow(output_image)                              #to plot the output image
+plt.show()                                            #output
