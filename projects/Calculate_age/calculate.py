488320811be29fee527ffa1858070dd9c3222b1e
Hamed-68 <hamedbakhti918@yahoo.com>
2021-03-16 21:15:12 +0330

    change month_days to smallest func

--- projects/Calculate_age/calculate.py
+++ projects/Calculate_age/calculate.py
@@ -12,11 +12,9 @@ def judge_leap_year(year):
 
 # returns the number of days in each month
 def month_days(month, leap_year):
-    if month == 1 or month == 3 or month == 5 or month == 7:
+    if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
-    elif month == 8 or month == 10 or month == 12:
-        return 31
-    elif month == 4 or month == 6 or month == 9 or month == 11:
+    elif month in [4, 6, 9, 11]:
         return 30
     elif month == 2 and leap_year:
         return 29
