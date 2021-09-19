4233e3a68f2862bdf909b9469fe0408567f5221a
hanshaoxiao <71498650+hanshaoxiao@users.noreply.github.com>
2021-09-19 17:26:21 +0800

    Update scrap-img.py
    debug and optimize

--- projects/Download_images_from_website/scrap-img.py
+++ projects/Download_images_from_website/scrap-img.py
@@ -4,56 +4,56 @@ import os
 from bs4 import BeautifulSoup
 import time
 
-# path= E:\web scraping\chromedriver_win32\chromedriver.exe
-path = input("Enter Path : ")
+path = "D:/webdriver/bin/chromedriver.exe"
+url = input("Enter URL:")
+output = "nature"
 
-url = input("Enter URL : ")
-
-output = "output"
-
-
-def get_url(path, url):
-    driver = webdriver.Chrome(executable_path=r"{}".format(path))
+def get_url(path,url):
+    driver = webdriver.Chrome(executable_path=path)
     driver.get(url)
-    print("loading.....")
+    print("loading...")
     res = driver.execute_script("return document.documentElement.outerHTML")
-
+    print("Got page Complete!")
     return res
 
-
 def get_img_links(res):
-    soup = BeautifulSoup(res, "lxml")
-    imglinks = soup.find_all("img", src=True)
+    soup = BeautifulSoup(res,"lxml")
+    imglinks = soup.find_all("img",src=True)
+    print("Get links Complete!")
     return imglinks
 
-
-def download_img(img_link, index):
+def download_img(imglink,index):
     try:
-        extensions = [".jpeg", ".jpg", ".png", ".gif"]
-        extension = ".jpg"
+        if "http" not in imglink:
+            imglink = "http:" + imglink
+
+        extensions = [".jpg",".jpeg",".png",".gif"]
+        extension = ""
         for exe in extensions:
-            if img_link.find(exe) > 0:
+            if imglink.find(exe):
                 extension = exe
                 break
 
-        img_data = rq.get(img_link).content
-        with open(output + "\\" + str(index + 1) + extension, "wb+") as f:
+        img_data = rq.get(imglink).content
+        with open(output + "/" + str(index + 1) + extension, "wb+") as f:
             f.write(img_data)
-        
-        f.close()
-    except Exception:
-        pass
+            f.close()
+    # get error
+    except Exception as e:
+        print("False type:",e.__class__.__name__)
+        print("False:",e)
 
-
-result = get_url(path, url)
+res = get_url(path,url)
 time.sleep(60)
-img_links = get_img_links(result)
+imglinks = get_img_links(res)
+
 if not os.path.isdir(output):
     os.mkdir(output)
 
-for index, img_link in enumerate(img_links):
-    img_link = img_link["src"]
+for index, img_link in enumerate(imglinks):
+    imglink = img_link["src"]
     print("Downloading...")
-    if img_link:
-        download_img(img_link, index)
-print("Download Complete!!")
+    if imglink:
+        download_img(imglink, index)
+
+print("Download Complete!")
