86077ce8b4667c6f3d1b49def45e57d5bf124010
Felipe Pacheco <felipepachecosa@gmail.com>
2021-09-20 17:15:57 +0100

    [FIX] Time error
    Fix error: Invalid Time Format

--- projects/whatsapp_Bot/main.py
+++ projects/whatsapp_Bot/main.py
@@ -6,7 +6,7 @@ now = datetime.now()
 chour = now.strftime("%H")
 mobile = input('Enter Mobile No of Receiver : ')
 message = input('Enter Message you wanna send : ')
-hour = int(chour) + int(input('Enter hour : '))
+hour = int(input('Enter hour : '))
 minute = int(input('Enter minute : '))
 
 pywhatkit.sendwhatmsg(mobile,message,hour,minute)
