2fdc7729e8376df1ab6182684947bc1792a416fc
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-10-23 15:32:46 +0530

    added the function which call the 4 language converters, compare the results from each and find the freequency of the results and return word with grater freequency (with atleast 2 as its freequency), and the dictionaries and its freequency. if all the words are with same freequency it returns 'NIL'

--- projects/Word Translator/WordTranslator.py
+++ projects/Word Translator/WordTranslator.py
@@ -18,3 +18,42 @@ def wordTranslate_lingee(origin, word):         #Word Translator Using Lingee
 def wordTranslate_pons(origin, word):           #Word Translator Using PONS
     translated_word = PonsTranslator(source = origin, target = 'english').translate(word)
     return translated_word
+
+
+def wordTranslate(origin, word): # Word Translator Main Function
+    word = word.strip()
+    translated_dict = {         #dictionary with the translated words using different API's
+        'google': wordTranslate_google(origin, word),
+        'myMemory': wordTranslate_myMemory(origin, word),
+        #'lingee': wordTranslate_lingee(origin, word),
+        'pons': wordTranslate_pons(origin, word)
+    }
+
+    translated_word_count = {}  #Creating a blank dictionaries for storing the frequency
+
+    for i in translated_dict.values():  #checking the freequency of the responses
+        count = 0
+        for j  in translated_dict.values():
+            if (i.lower()).strip() == (j.lower()).strip():
+                count+=1
+            else:
+                pass
+        if i not in translated_word_count.keys():
+            translated_word_count[i] = (count)
+        else:
+            pass    
+
+    translated_word = ''
+    max_count = 0
+    for key, value in translated_word_count.items(): #finding the translated word with highest freequency
+        
+        if max_count < value :
+            max_count = value
+            if value >= 2: #only select words with atleast freequency of 2
+                translated_word = str(key)
+            else:
+                translated_word = 'NIL'  #storing the translated word
+        else:
+            pass
+
+    return translated_word, translated_dict, max_count, translated_word_count #returning the translated word
