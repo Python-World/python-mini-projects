610d70001559c7d26e9635a01f66baef649252bd
Back-end and front-end developer <71826040+RgTheGreat@users.noreply.github.com>
2021-10-25 10:14:43 +0530

    Created a text to morse code translator

---
+++ projects/text_to_morse_code/text_to_morse_code.py
@@ -0,0 +1,43 @@
+#all_the symbols
+symbols = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": ".-",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
+#the user has to type a word
+ask = input("type: ")
+
+
+length = len(ask)
+output = ""
+
+#logic
+for i in range(length):
+    if ask[i] in symbols.keys():
+        output = output + " " + symbols.get(ask[i])
+
+print(output)
