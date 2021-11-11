5223343d4ef82cb754f52c283ea54e3a6649c2d3
Miguel Wychovaniec <mwychovaniec@segware.com>
2021-11-11 18:57:18 -0300

    Added requirements.txt

--- projects/S3_File_Upload/main.py
+++ projects/S3_File_Upload/main.py
@@ -8,6 +8,7 @@ BUCKET_NAME = 'bucket_name'
 S3_FILE_NAME = 'file_name_on_s3'
 
 def upload_to_s3(local_file, bucket, s3_file):
+    ## This function is responsible for uploading the file into the S3 bucket using the specified credentials. 
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
     try:
