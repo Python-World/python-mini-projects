a76963a28e94ba7654147d83776b9487d40905ad
echoaj <aljoslin13@yahoo.com>
2021-11-17 23:27:48 -0800

    new comment in sine wave file

--- projects/Sine_Wave/sine_wave.py
+++ projects/Sine_Wave/sine_wave.py
@@ -10,6 +10,7 @@ D = 0       # Vertical Shift
 penup()
 # As x increases y increases and decreases as it is evaluated.
 for x in range(-200, 200):
+    # Sine Wave Equation
     y = A * sin((2 * pi / B) * (x + C)) + D
     goto(x, y)
     pendown()
