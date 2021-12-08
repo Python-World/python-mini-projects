84ad8ad839ca1dbc5a018ceffd6db65db3e479d7
Charitha Reddy <95674159+Charitha1808@users.noreply.github.com>
2021-12-08 13:02:26 +0530

    Update generate_qrcode.py

--- projects/Qr_code_generator/generate_qrcode.py
+++ projects/Qr_code_generator/generate_qrcode.py
@@ -1,18 +1,16 @@
-import qrcode
+# Import QRCode from pyqrcode
+import pyqrcode
+import png
+from pyqrcode import QRCode
 
-input_URL = "https://www.google.com/"
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=15,
-    border=4,
-)
+# String which represents the QR code
+s = "www.iare.ac.in"
+# Generate QR code
+url = pyqrcode.create(s)
 
-qr.add_data(input_URL)
-qr.make(fit=True)
+# Create and save the svg file naming "myqr.svg"
+url.svg("myqr.svg", scale = 8)
 
-img = qr.make_image(fill_color="red", back_color="white")
-img.save("url_qrcode.png")
-
-print(qr.data_list)
+# Create and save the png file naming "myqr.png"
+url.png('myqr.png', scale = 6)
