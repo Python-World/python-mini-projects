61ee31ec427fbcae03401e52739b482dfb25f8bd
Prajwal Kedari <65669641+prajwalkedari@users.noreply.github.com>
2021-12-26 23:32:40 +0530

    Odd or even and  prime or nor

--- projects/Odd or Even and Prime or Not/Main.py
+++ projects/Odd or Even and Prime or Not/Main.py
@@ -7,19 +7,21 @@ GitHub: prajwalkedari , prajwal-kedari
 Link: https://www.github.com/prajwalkedari/
 Source: Python-world
 Thank!!!
-''''
+'''
 
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
-   print(f"{num} is Even")
+   Odd_or_even_result=f"{num} is Even as well as"
 else:
-   print(f"{num} is Odd")
+   Odd_or_even_result=f"{num} is Odd as well as"
 if num > 1: 
     for i in range(2, int(num/2)+1): 
         if (num % i) == 0: 
-            print(num, "is not a prime number") 
+            prime_or_not_result=" nor  prime number" 
             break
     else: 
-        print(num, "is a prime number") 
+        prime_or_not_result=" prime number"
 else: 
-    print(num, "is not a prime number")
+    prime_or_not_result=" nor prime number"
+
+print(Odd_or_even,prime_or_not_result)
