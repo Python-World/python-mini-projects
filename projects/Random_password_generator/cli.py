e5717cf6e3f2b2aa023773ac95840095498651aa
MisanoGo <94430623+MisanoGo@users.noreply.github.com>
2022-02-19 20:39:00 +0330

    fix bug

--- projects/Random_password_generator/cli.py
+++ projects/Random_password_generator/cli.py
@@ -1,29 +1,33 @@
 from psw import password_generator, PasswordSecurityLevel as PSL
 from pyperclip import copy
 
-y = 'y'
+def main():
+    y = 'y'
 
-print('Hello World !')
-print('Wellcome to password generator')
-print('------------------------------')
+    print('Hello World !')
+    print('Wellcome to password generator')
+    print('------------------------------')
 
-while(True):
-    level = input('\n < Enter level of password security > [a/b/c/d/e] : ')
-    lenght = int(input(' < Enter lenght of password > : '))
-    psw_level = PSL.get(level)
+    while(True):
+        level = input('\n < Enter level of password security > [a/b/c/d/e] : ')
+        lenght = int(input(' < Enter lenght of password > : '))
+        psw_level = PSL.get(level)
 
-    psw = password_generator(psw_level, lenght)
+        psw = password_generator(psw_level, lenght)
 
-    print(" < your random passwoed > : ", psw)
+        print(" < your random passwoed > : ", psw)
 
-    copy_condition = input(
-        " would you like to copy it in your clipboard ? [y/n] : ")
-    if copy_condition == y:
-        copy(psw)
-        print(' done!')
+        copy_condition = input(
+            " would you like to copy it in your clipboard ? [y/n] : ")
+        if copy_condition == y:
+            copy(psw)
+            print(' done!')
 
-    continue_condition = input(" continue ? [y/n] : ")
-    if continue_condition != y:
-        break
+        continue_condition = input(" continue ? [y/n] : ")
+        if continue_condition != y:
+            break
 
-print('so long.')
+    print('so long.')
+
+if __name__ == '__main__':
+    main()
