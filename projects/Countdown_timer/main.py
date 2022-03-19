3c32ec10d647a55fb96f03bfb676b26787ea7978
KelvinQiu <kelvinqiu802@outlook.com>
2022-03-19 14:41:12 +0800

    countdown_plus

--- projects/Countdown_timer/main.py
+++ projects/Countdown_timer/main.py
@@ -1,14 +1,23 @@
 import time
 
+
 def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins,secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
+    while int(t):
+        if t > 10:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
+        else:  # less than 10 seconds
+            secs, ms = divmod(t * 100, 100)
+            timer = '{:02d}:{:02d}:{:02d}'.format(0, int(secs), int(ms))
+            print(timer, end="\r")
+            time.sleep(0.01)
+            t -= 0.01
 
     print('Timer completed!')
+
 
 t = input('Enter the time in seconds: ')
 
